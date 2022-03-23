1e20545f4cdc36b3e9b263a6278bc99679b1b0d9
Marcos Victor <marcosvictorsb@gmail.com>
2022-03-22 22:39:48 -0300

    chore(status-code): added const status code in controllers

--- src/domains/customer/controllers/customer-controller.ts
+++ src/domains/customer/controllers/customer-controller.ts
@@ -1,28 +1,30 @@
+
 import { Request, Response } from "express";
-import CustomerRepository from "../repository/customer-repository";
+import { HttpStatusCode, HttpResponse } from "../../../protocols/http";
 import CustomerService from "../services/customer-service";
+
 
 const service = new CustomerService()
 
 export default class CustomerController {
   
-    getCustomer(request: Request, response: Response): any {
+    getCustomers(request: Request, response: Response): any {        
         try {
-            const customer = service.getCustomer();
-            response.status(200).json({customer})     
+            const customers = service.getCustomers();
+            return response.status(HttpStatusCode.OK).json({ customers})          
         } catch (error) {
-            response.status(500).json({message: error})     
+            return response.status(HttpStatusCode.INTERNAL_SERVER_ERROR).json({ message: error})          
         }
-       
+        
     }
 
     create(request: Request, response: Response): any {
         try {
             const { name } = request.body;
             const customer = service.create(name);
-            response.status(201).json({customer}) 
+            return response.status(HttpStatusCode.CREATED).json({ customer }) 
         } catch (error) {
-            response.status(500).json({message: error})     
+            return response.status(HttpStatusCode.INTERNAL_SERVER_ERROR).json({message: error})     
         }
     }
 }
