5105f76bce2823524e25c9a8c761bf72ac001e52
Marcos Victor <marcosvictorsb@gmail.com>
2022-03-27 10:41:25 -0300

    feat(model): create model customer

---
+++ src/models/customer.ts
@@ -0,0 +1,41 @@
+'use strict';
+import { Model } from 'sequelize';
+
+
+interface CustomerAttributes {
+  name: string,
+  email: string,
+  password: string
+}
+
+module.exports = (sequelize: any, DataTypes:any) => {
+  class Customer extends Model<CustomerAttributes> implements CustomerAttributes {
+    name!: string;
+    email!: string;
+    password!: string;
+
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models: any) {
+      // define association here
+    }
+  }
+  Customer.init({
+    name:  {
+      type: DataTypes.STRING,
+    },
+    email: {
+      type: DataTypes.STRING,
+    },
+    password: {
+      type: DataTypes.STRING,
+    } 
+  }, {
+    sequelize,
+    modelName: 'Customer',
+  });
+  return Customer;
+};
