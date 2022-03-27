fdfee4ddbf8b295b1d0dfa3de649c1f06c0e0a26
Marcos Victor <marcosvictorsb@gmail.com>
2022-03-27 10:39:35 -0300

    chore(controller): crate controller to get and created new customer, and validator bad request when create new customer

--- src/domains/customer/controllers/customer-controller.ts
+++ src/domains/customer/controllers/customer-controller.ts
@@ -1,28 +1,40 @@
 
 import { Request, Response } from "express";
-import { HttpStatusCode, HttpResponse } from "../../../protocols/http";
+import { HttpStatusCode, HttpResponse } from "../../../protocols/";
 import CustomerService from "../services/customer-service";
+import { customerSchema } from "../validator";
 
 
 const service = new CustomerService()
 
 export default class CustomerController {
-  
-    getCustomers(request: Request, response: Response): any {        
-        try {
-            const customers = service.getCustomers();
-            return response.status(HttpStatusCode.OK).json({ customers})          
+
+    async getCustomers(request: Request, response: Response): Promise<any> {        
+        try {            
+            const customers = await service.getCustomers();
+            
+            return response.status(HttpStatusCode.OK).json({ customers});      
         } catch (error) {
-            return response.status(HttpStatusCode.INTERNAL_SERVER_ERROR).json({ message: error})          
-        }
-        
+            console.log(error)
+            return response.status(HttpStatusCode.INTERNAL_SERVER_ERROR).json({ message: error});      
+        }        
     }
 
-    create(request: Request, response: Response): any {
-        try {
-            const { name } = request.body;
-            const customer = service.create(name);
-            return response.status(HttpStatusCode.CREATED).json({ customer }) 
+    async create(request: Request, response: Response): Promise<any> {
+        try {            
+            const { name, email, password } = request.body;
+            const customer = {
+                name,
+                email,
+                password
+            }
+            const schema = customerSchema.validate(customer);
+            if (schema.error) {
+                return response.status(HttpStatusCode.BAD_REQUEST).json(schema.error.details)
+            }
+
+            const newCustomer = await service.create(customer);
+            return response.status(HttpStatusCode.CREATED).json({ newCustomer }) 
         } catch (error) {
             return response.status(HttpStatusCode.INTERNAL_SERVER_ERROR).json({message: error})     
         }
