68a756bf959fa642c1cc9d71c01683774404f9e4
Marcos Victor <marcosvictorsb@gmail.com>
2022-03-31 07:56:42 -0300

    chore(route): remove route attendatRoute

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,7 @@
 import customerRouter from "../domains/customer/routes";
-import attendantRouter from "../domains/attendant/routes";
 
 
 
 export const routes = {
   customerRouter,
-  attendantRouter
 }
