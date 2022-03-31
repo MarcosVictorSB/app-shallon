23fbbf4f6f166e3d74a5b92d579798d818b58546
Marcos Victor <marcosvictorsb@gmail.com>
2022-03-31 07:24:26 -0300

    feat(package): npm install bcryptjs

--- src/domains/attendant/controllers/index.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response } from "express";
-export default class AttendantController {
-  getAttendants(request: Request, response: Response) {
-    response.status(200).json({ message: 'nova rota'})
-  }
-}
