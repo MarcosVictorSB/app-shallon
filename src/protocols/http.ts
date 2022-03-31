6eb38170e6eceadcf9078399bcaf7f40780ec0ff
Marcos Victor <marcosvictorsb@gmail.com>
2022-03-30 22:45:04 -0300

    chore(status-code): added news status codes (conflicts, notfound, forbidden)

--- src/protocols/http.ts
+++ src/protocols/http.ts
@@ -2,6 +2,9 @@ export enum HttpStatusCode {
     OK = 200,
     CREATED = 201,
     BAD_REQUEST = 400,
+    FORBIDDEN = 403,
+    NOT_FOUND = 404,
+    CONFLICT = 409,
     INTERNAL_SERVER_ERROR = 500
 }
 
@@ -9,3 +12,4 @@ export type HttpResponse = {
     statusCode: number,
     body: any;
 }
+
